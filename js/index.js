434b0cca979477a24fd846d25e975afd17f4622e
Raphael Rychard <raph.rych@gmail.com>
2023-03-19 23:27:53 -0400

    add Title e index
    create Tegged Templad String

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-// Array
+// PEGA A DIV ROOT DA TELA
+const root = document.querySelector('#root')
 
-const nome = ["Raphael", "Rychard"]
-const nomes = ['SeuDerick', 'Lucas', 'Ansiente', 'Ryazero', 'Carol', 'Raquel']
+//DECLARA A CONTANTE PARA PEGA O CONTEÚDO
+const textTitle = 'Dificuldade'
 
-console.log(nomes[1])
 
+/* 
+    Tagged Tamplade String
+    -> É UMA FUNÇÃO QUE AGENTE PASSA NA FRENTE DELA UMA Tamplade String
+
+        NESSA TEMPLADE STRING, A gente pode usar String e Interpolando quando a gente quiser variaveis ou métodos JavaScript ou Uma String do JavaScript
+
+*/
+const title = Title`
+    text-align: center; 
+    color: #f6f6f6; 
+    padding: 20px;
+    font-size: 3em;
+    background: royalblue;
+    ${textTitle}
+`
+
+
+root.insertAdjacentHTML("beforeend", title)
 
